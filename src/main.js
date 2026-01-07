1eb15724733d389162f38687eb25c2ac0e1fcdc4
pfogued <pablitocalamocha05@gmail.com>
2026-01-08 00:03:35 +0100

    ultimo (deberia)

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import { useAuthStore } from './stores/auth'
 
 // CONFIGURACIÓN CORREGIDA:
 // Quitamos el /api del final para que no se duplique en las llamadas de los componentes
-axios.defaults.baseURL = 'http://shuttle.proxy.rlwy.net:29298'
+axios.defaults.baseURL = 'http://ballast.proxy.rlwy.net:37892'
 axios.defaults.withCredentials = true
 
 const app = createApp(App)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
       <!-- Encabezado -->
       <div class="text-center">
         <h2 class="text-3xl font-extrabold text-gray-900">Iniciar Sesión</h2>
-        <p class="mt-2 text-sm text-gray-600">Accede a tu cuenta de InkTattooPro (RF-2)</p>
+        <p class="mt-2 text-sm text-gray-600">Accede a tu cuenta de InkTattooPro</p>
       </div>
 
       <!-- Formulario de Login -->
